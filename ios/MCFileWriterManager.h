88030e10737535a5589ff80e4ac8f125fe394fcf
Brice Mason <bricemason@gmail.com>
2017-03-07 11:43:12 -0500

    Initial commit

---
+++ ios/MCFileWriterManager.h
@@ -0,0 +1,14 @@
+//
+//  MCFileWriterManager.h
+//  CustomModule
+//
+//  Created by Brice Mason on 3/7/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface MCFileWriterManager : NSObject <RCTBridgeModule>
+
+@end
